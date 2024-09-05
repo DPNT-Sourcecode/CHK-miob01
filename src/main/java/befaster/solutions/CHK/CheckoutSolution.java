cee505b0149d1658eb452955a2957959cf9079a7
sbx_user1051 <sbx_user1051@169.254.119.253>
2024-09-05 19:41:20 +0000

    Thu Sep 05 19:41:20 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,84 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+class Item{
+    int price;
+    String name;
+    int specialOfferQuantity;
+    int specialOfferPrice;
+
+    public Item(String name, int price, int specialOfferQuantity, int specialOfferPrice) {
+        this.price = price;
+        this.name = name;
+        this.specialOfferQuantity = specialOfferQuantity;
+        this.specialOfferPrice = specialOfferPrice;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSpecialOfferQuantity() {
+        return specialOfferQuantity;
+    }
+
+    public void setSpecialOfferQuantity(int specialOfferQuantity) {
+        this.specialOfferQuantity = specialOfferQuantity;
+    }
+
+    public int getSpecialOfferPrice() {
+        return specialOfferPrice;
+    }
+
+    public void setSpecialOfferPrice(int specialOfferPrice) {
+        this.specialOfferPrice = specialOfferPrice;
     }
 }
+
+public class CheckoutSolution {
+
+    private final HashMap<String,Item> storeItems = new HashMap<>();
+    private final HashMap<String,Integer> inventoryItems = new HashMap<>();
+
+    private void initItems(){
+        storeItems.put("A", new Item("A",50,3,130));
+        storeItems.put("B", new Item("B",30,2,45));
+        storeItems.put("C", new Item("C",20,0,0));
+        storeItems.put("D", new Item("D",15,0,0));
+    }
+
+    public Integer checkout(String skus) {
+        int sumToPay = 0;
+        String[] itemNames = skus.split("[,\\s]+");
+        for (String itemName: itemNames){
+            inventoryItems.put(itemName,inventoryItems.getOrDefault(itemName,0)+1);
+        }
+        for (Map.Entry<String,Integer> entry : inventoryItems.entrySet()){
+            if (!storeItems.containsKey(entry.getKey())){
+                return -1;
+            }
+            Item item = storeItems.get(entry.getKey());
+            if (item.specialOfferQuantity == 0){
+                sumToPay += (entry.getValue()/item.specialOfferQuantity) * item.specialOfferPrice + item.price * entry.getValue() % item.specialOfferQuantity;
+            } else{
+                sumToPay += entry.getValue() * item.price;
+            }
+        }
+        return sumToPay;
+    }
+}
+
