32c087fa5dfd2c376156719fb26ffa39b15595a3
sbx_user1051 <sbx_user1051@169.254.51.165>
2024-09-06 08:49:38 +0000

    Fri Sep 06 08:49:38 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,30 +2,30 @@ package befaster.solutions.CHK;
 
 import java.util.*;
 
-class SpecialOfferPair<K,V>{
-    private final K quantity;
-    private final V price;
+class SpecialOfferPair{
+    private final Integer quantity;
+    private final Object price;
 
-    public SpecialOfferPair(K quantity, V price) {
+    public SpecialOfferPair(Integer quantity, Object price) {
         this.quantity = quantity;
         this.price = price;
     }
 
-    public K getQuantity() {
+    public Integer getQuantity() {
         return quantity;
     }
 
-    public V getPrice() {
+    public Object getPrice() {
         return price;
     }
 }
 
-class Item<K,V>{
+class Item{
     private int price;
     private String name;
-    private PriorityQueue<SpecialOfferPair<K,V>> offers;
+    private PriorityQueue<SpecialOfferPair> offers;
 
-    public Item(String name, int price, PriorityQueue<SpecialOfferPair<K,V>> offers) {
+    public Item(String name, int price, PriorityQueue<SpecialOfferPair> offers) {
         this.price = price;
         this.name = name;
         this.offers = offers;
@@ -47,11 +47,11 @@ class Item<K,V>{
         this.name = name;
     }
 
-    public PriorityQueue<SpecialOfferPair<K, V>> getOffers() {
+    public PriorityQueue<SpecialOfferPair> getOffers() {
         return offers;
     }
 
-    public void setOffers(PriorityQueue<SpecialOfferPair<K, V>> offers) {
+    public void setOffers(PriorityQueue<SpecialOfferPair> offers) {
         this.offers = offers;
     }
 }
@@ -61,19 +61,19 @@ public class CheckoutSolution {
     private final HashMap<String,Integer> inventoryItems = new HashMap<>();
     private Map<String,Item> storeItems;
     private void initStore(){
-        PriorityQueue<SpecialOfferPair<Integer, Integer>> offersForA = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair<Integer, Integer>::getQuantity).reversed());
-        offersForA.add(new SpecialOfferPair<>(3, 130));
-        offersForA.add(new SpecialOfferPair<>(5, 200));
-        PriorityQueue<SpecialOfferPair<Integer, Integer>> offersForB = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair<Integer, Integer>::getQuantity).reversed()) {
+        PriorityQueue<SpecialOfferPair> offersForA = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair::getQuantity).reversed());
+        offersForA.add(new SpecialOfferPair(3, 130));
+        offersForA.add(new SpecialOfferPair(5, 200));
+        PriorityQueue<SpecialOfferPair> offersForB = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair::getQuantity).reversed()) {
         };
-        offersForB.add(new SpecialOfferPair<>(2, 45));
-        PriorityQueue<SpecialOfferPair<Integer, String>> offersForE = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair<Integer, String>::getQuantity).reversed()) {
+        offersForB.add(new SpecialOfferPair(2, 45));
+        PriorityQueue<SpecialOfferPair> offersForE = new PriorityQueue<>(Comparator.comparing(SpecialOfferPair::getQuantity).reversed()) {
         };
-        offersForE.add(new SpecialOfferPair<>(2,"B"));
+        offersForE.add(new SpecialOfferPair(2,"B"));
 
         storeItems = Map.of("A", new Item("A",50, offersForA),
                 "B", new Item("B",30, offersForB),
-                "C", new Item("C",20,new PriorityQueue<>()),
+                "C", new Item("C",20, new PriorityQueue<>()),
                 "D", new Item("D",15,new PriorityQueue<>()),
                 "E", new Item("E",40, offersForE));
     }
@@ -105,11 +105,16 @@ public class CheckoutSolution {
         return sumToPay;
     }
 
-    private int getSumForProduct(String product){
+    private int getSumForProduct(String product, int quantity){
         Item item = storeItems.get(product);
-        for (SpecialOfferPair specialOfferPair : item.getOffers()){
-
+        for (SpecialOfferPair specialOfferPair : item.getOffers().stream().toList()){
+            if (specialOfferPair.getQuantity() != 0  ){
+                sumToPay += (entry.getValue()/item.specialOfferQuantity) * item.specialOfferPrice + item.price * (entry.getValue() % item.specialOfferQuantity);
+            } else{
+                sumToPay += entry.getValue() * item.price;
+            }
         }
 
     }
 }
+
